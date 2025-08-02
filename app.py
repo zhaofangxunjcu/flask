7954a6da6e9860fda516e1fe156067fef141d76c
zfz20 <fangxun.zhao@my.jcu.edu.au>
2025-08-02 21:36:54 +0800

    add convert function

--- app.py
+++ app.py
@@ -2,14 +2,24 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+def celsius_to_fahrenheit(celsius):
+    return float(celsius) * 9 / 5 + 32
+
 @app.route('/')
-def hello_world():
+def home():
     return '<h1>Hello World :)</h1>'
 
-@app.route('/greet')
+app.route('/greet')
 @app.route('/greet/<name>')
 def greet(name=""):
     return f"Hello {name}"
 
+
+@app.route('/f/<celsius>')
+def convert(celsius):
+    fahrenheit = celsius_to_fahrenheit(celsius)
+    return str(round(fahrenheit, 2))
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
