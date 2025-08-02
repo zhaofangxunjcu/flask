7b3b449a4859680bb0ac246c774e2f4fc28c7f2c
zfz20 <fangxun.zhao@my.jcu.edu.au>
2025-08-02 21:33:51 +0800

    add greet name

--- app.py
+++ app.py
@@ -5,8 +5,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return '<h1>Hello World :)</h1>'
+
 @app.route('/greet')
-def greet():
-    return "Hello"
+@app.route('/greet/<name>')
+def greet(name=""):
+    return f"Hello {name}"
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
